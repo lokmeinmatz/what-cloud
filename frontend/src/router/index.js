2af3b7f2b72b2894d69c001f69c48714edd45bb9
lokmeinmatz <matze.kind@web.de>
2020-07-02 11:48:33 +0200

    added files routing

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Files from '../views/Files.vue'
 
 Vue.use(VueRouter)
 
@@ -12,11 +13,13 @@ Vue.use(VueRouter)
   },
   {
     path: '/files',
+    alias: '/files/*',
     name: 'Files',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Files.vue')
+    //component: () => import(/* webpackChunkName: "about" */ '../views/Files.vue')
+    component: Files
   }
 ]
 
--- frontend/src/views/Files.vue
+++ frontend/src/views/Files.vue
@@ -1,16 +1,46 @@
 <template>
   <div class="content container-sm">
     <main>
-      <h1>See all your files</h1>
-      <virtual-list></virtual-list>
+      <h1 v-if="isRoot">All files</h1>
+      <div v-else class="btn-group" role="group" aria-label="File path">
+        <router-link 
+          v-for="(elmt, index) in subPath"
+          :key="elmt.filePath"
+          :to="`/files${elmt.filePath}`"
+          :class="['btn',  (index + 1 != subPath.length) ? 'btn-secondary' : 'btn-primary']"
+          >
+        ▶ {{elmt.segment}}
+        </router-link> 
+      </div>
     </main>
     <aside></aside>
   </div>
 </template>
 <script>
-import VirtualList from 'vue-virtual-scroll-list'
 
 export default {
-  components: { 'virtual-list': VirtualList }
+  mounted() {
+    console.log(this.$route.path.substr(6))
+  },
+  data() {
+    return {
+      files: []
+    }
+  },
+  computed: {
+    subPath() {
+      const segments = this.$route.path.substr(7).split('/').filter(e => e.trim().length > 0)
+      if (segments.length == 0) return []
+      return segments.reduce(([collector, prevPath], curr) => {
+        const npath = `${prevPath}/${curr}`
+        collector.push({segment: curr, filePath: npath})
+        return [collector, npath]
+      }, [[], ''])[0]
+    },
+
+    isRoot() {
+      return this.subPath.length == 0
+    }
+  }
 }
 </script>
